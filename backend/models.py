e415b1cc24d7a6da17c2f60d6f66840c7c7e57c6
majid <majid21va@gmail.com>
2025-03-18 15:10:22 +0500

    model updated

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import timedelta
 
-# Create your models here.
+class Product(models.Model):
+    image = models.ImageField(upload_to='uploads/images', default='uploads/images/default.jpg')
+    name = models.CharField(max_length=200, blank=False)
+    category = models.CharField(max_length=100, blank=False)
+    starting_bid = models.DecimalField(max_digits=6, decimal_places=2)
+    bid_start_date = models.DateTimeField(auto_now_add=True)
+    auction_duration = models.DurationField(default=timedelta(hours=24))  # Default to 24 hours
+
+    def bid_end_date(self):
+        """Calculate auction end time based on bid start date and duration."""
+        return self.bid_start_date + self.auction_duration
+
+    def __str__(self):
+        return self.name
