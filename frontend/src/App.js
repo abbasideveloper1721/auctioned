99370d94813b48c1cd8c934d9fa5293d66d7294b
majid <majid21va@gmail.com>
2025-03-21 15:05:18 +0500

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
+import addproducts from './components/addproducts';
 import ShowProducts from './components/showproducts';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-         <ShowProducts/>
+         <Router>
+          <Routes>
+            <Route exact path='/' Component={ShowProducts}></Route>
+            <Route exact path='/addproduct' Component={addproducts}></Route>
+          </Routes>
+         </Router>
     </div>
   );
 }
