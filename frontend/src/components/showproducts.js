3bb9e2c4afe109b945b6792c2a464d9e0c82498e
majid <majid21va@gmail.com>
2025-03-24 01:47:20 +0500

    api fethching

--- frontend/src/components/showproducts.js
+++ frontend/src/components/showproducts.js
@@ -7,7 +7,7 @@ const ShowProducts = () => {
 const [products,setProducts] = useState([])
 const getProducts = async() =>{
     const response = await axios.get('http://127.0.0.1:8000/api/')
-    console.log(response.data)
+    setProducts(response.data)
 
 }
 useEffect(()=> {
@@ -18,7 +18,25 @@ useEffect(()=> {
 
 
     return(
-        <div>Show all products</div>
+        <div>
+            <h1>Show all products</h1>
+        {
+            products.map((products,index) => (
+                <div>
+                    <img src={products.image}></img>
+                    <p>{products.name}</p>
+                    <p>{products.category}</p>
+                    <p>{products.starting_bid}</p>
+                    <p>{products.bid_start_date}</p>
+                    <p>{products.auction_duration}</p>
+                    <p>{products.bid_end_date}</p>
+                    <p>{products.time_left}</p>
+                    <p>{products.status}</p>
+                </div>
+            ))
+        }
+
+        </div>
     );
 };
 export default ShowProducts;
